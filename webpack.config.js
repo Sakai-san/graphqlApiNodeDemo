b7e9b4f0736061f3b7beb14b3c5ea1f14ab6c246
Sakai-san <o_edo@hotmail.com>
2018-08-18 14:42:21 +0200

    Simplified the compilation process.

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './app/public/js/index.js',
-  output: {
-    filename: 'bundle.min.js',
-    path: path.resolve(__dirname, 'app/public/js')
-  }
-};
