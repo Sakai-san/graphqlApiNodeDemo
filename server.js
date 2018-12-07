036b60cce984d866b4ba5a4f2dd028ff1a2c8555
Sakai-san <o_edo@hotmail.com>
2018-12-07 09:17:43 +0100

    Reduce amont of imported libraries.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-import faker from 'faker';
-import lodash from 'lodash';
-import typeDefs from './schema';
-import resolvers from './resolvers';
-import db from './models';
-
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import faker from "faker";
+import times from "lodash.times";
+import random from "lodash.random";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+import db from "./models";
 
 const server = new ApolloServer({
   typeDefs: gql(typeDefs),
@@ -16,26 +16,26 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.use(express.static('app/public'));
+app.use(express.static("app/public"));
 
 db.sequelize.sync().then(() => {
   // populate author table with dummy data
   db.author.bulkCreate(
-    lodash.times(10, () => ({
+    times(10, () => ({
       firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-    })),
+      lastName: faker.name.lastName()
+    }))
   );
   // populate post table with dummy data
   db.post.bulkCreate(
-    lodash.times(10, () => ({
+    times(10, () => ({
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
-      authorId: lodash.random(1, 10),
-    })),
+      authorId: random(1, 10)
+    }))
   );
 
   app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
   );
 });
