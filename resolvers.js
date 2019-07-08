0779470962ed3f1a8dca01ced4d9a5c7741d9bfd
Sakai Toma <o_edo@hotmail.com>
2019-07-08 18:40:48 +0200

    Replace deprecated findById function.

--- resolvers.js
+++ resolvers.js
@@ -1,15 +1,15 @@
 export default {
   Author: {
-    posts: (parent, args, context, info) => parent.getPosts(),
+    posts: (parent, args, context, info) => parent.getPosts()
   },
   Post: {
-    author: (parent, args, context, info) => parent.getAuthor(),
+    author: (parent, args, context, info) => parent.getAuthor()
   },
   Query: {
     posts: (parent, args, { db }, info) => db.post.findAll(),
     authors: (parent, args, { db }, info) => db.author.findAll(),
-    post: (parent, { id }, { db }, info) => db.post.findById(id),
-    author: (parent, { id }, { db }, info) => db.author.findById(id) 
+    post: (parent, { id }, { db }, info) => db.post.findByPk(id),
+    author: (parent, { id }, { db }, info) => db.author.findByPk(id)
   },
   Mutation: {
     createPost: (parent, { title, content, authorId }, { db }, info) =>
@@ -19,16 +19,18 @@ export default {
         authorId: authorId
       }),
     updatePost: (parent, { title, content, id }, { db }, info) =>
-      db.post.update({
-        title: title,
-        content: content
-      },
-      {
-        where: {
-          id: id
+      db.post.update(
+        {
+          title: title,
+          content: content
+        },
+        {
+          where: {
+            id: id
+          }
         }
-      }),
-    deletePost: (parent, {id}, { db }, info) =>
+      ),
+    deletePost: (parent, { id }, { db }, info) =>
       db.post.destroy({
         where: {
           id: id
